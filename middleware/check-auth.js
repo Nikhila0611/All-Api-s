67ceb95b99e03885db6a03789772f373b233035a
Nikhila0611 <134033000+Nikhila0611@users.noreply.github.com>
2024-02-15 12:41:23 +0530

    Update check-auth.js

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,15 +1,20 @@
 const jwt = require('jsonwebtoken');
 
+module.exports = (req, res, next) => {
+    try {
+        console.log(req.headers);
 
-module.exports = (req,res,next) =>{
-   
-    try{
-    const token = req.headers.authorization.split(' ')[1];
-    const decode = jwt.verify(token,"webBatch");
-    req.userData = decode;
-    // return res.json(decode);
-    next();
-    }catch(error){
-        res.json({success:false,message:"Auth failed"})
+        if (!req.headers.authorization) {
+            throw new Error('Authorization header is missing');
+        }
+
+        const token = req.headers.authorization.split(' ')[1];
+        const decode = jwt.verify(token, "webBatch");
+        req.userData = decode;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(401).json({ success: false, message: "Auth failed: Invalid token" });
     }
-}
+};
+
