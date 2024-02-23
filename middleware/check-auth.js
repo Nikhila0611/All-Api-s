025d72ca53421dba142e10c56cb6af16bf7e8238
Nikhila0611 <134033000+Nikhila0611@users.noreply.github.com>
2024-02-23 15:58:40 +0530

    Add files via upload

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,12 +2,6 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        console.log(req.headers);
-
-        if (!req.headers.authorization) {
-            throw new Error('Authorization header is missing');
-        }
-
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, "webBatch");
         req.userData = decode;
